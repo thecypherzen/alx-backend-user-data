d48f4ca86d411ac34039e2697371e386e9a201f7
William Inyam <sw.inyam@outlook.com>
2024-11-17 15:11:19 +0000

    build(0x01:api): add basic_auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module defining BasicAuth
+"""
+
+from api.v1.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class
+
+    Empty for now
+    """
+    pass
