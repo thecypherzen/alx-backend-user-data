6857ddc8b69daf2d14e697b3979328357a345b67
William Inyam <sw.inyam@outlook.com>
2024-11-18 14:32:21 +0000

    feat(0x02:session_auth): add current_user method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -36,3 +36,14 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Fetches user instance from cookie value
+        """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+
+        from models.user import User
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
