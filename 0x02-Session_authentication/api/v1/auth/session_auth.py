13d80941829c1746c31febc0cb602743c7abb4ec
William Inyam <sw.inyam@outlook.com>
2024-11-18 09:50:04 +0000

    feat(0x02:auth): enable retrieving user_id by session_id in session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,10 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """Creates session_id for user_id
+
+        Returns:
+           - None if user_id is None or not a string
+           - new session id
         """
         if not user_id or not isinstance(user_id, str):
             return None
@@ -20,3 +24,15 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Fetches a user_id based on session_id
+
+        Returns:
+           - None if session_id is None or not a string
+           - the user_id for given session_id as contained in
+             the <user_id_by_session_id> dictionary
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
