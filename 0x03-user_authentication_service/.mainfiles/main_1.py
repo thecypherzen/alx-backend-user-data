d03b75ca96681b5b67d96caae65cb04c482b5ab0
William Inyam <sw.inyam@outlook.com>
2024-11-21 14:53:22 +0000

    test(0x02:db): add test for add_user method

--- 0x03-user_authentication_service/.mainfiles/main_1.py
+++ 0x03-user_authentication_service/.mainfiles/main_1.py
@@ -14,3 +14,10 @@ print(user_1.id)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
+
+user3 = my_db.get_user(user_2.id)
+print(getattr(user3, "id", None))
+print(getattr(user3, "email", None))
+print(getattr(user3, "hashed_password", None))
+print(getattr(user3, "session_id", None))
+print(getattr(user3, "reset_token", None ))
