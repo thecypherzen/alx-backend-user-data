6acb2e0a4c09bf5776698acf00a75f9a5292d393
William Inyam <sw.inyam@outlook.com>
2024-11-22 00:16:22 +0000

    feat(0x03:auth): add valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,20 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if credentials match that of a user
+
+        Params:
+           - email(str): the user's email
+           - password(str): the user's password
+        Returns:
+           - True if user by email exist and password matches the user
+           - False otherwise
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
+        except Exception:
+            return False
