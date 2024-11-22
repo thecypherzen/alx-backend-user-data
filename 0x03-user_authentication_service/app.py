f1e639f675acfc57a441f81c1db712bddca97ba6
William Inyam <sw.inyam@outlook.com>
2024-11-22 00:03:03 +0000

    feat(0x02:app): add users post route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Flask Application"""
+from auth import Auth
+from flask import Flask, jsonify, request
 
-from flask import Flask, jsonify
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +13,22 @@ def index():
     """GET: /
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST: /users
+    Creates and Adds a new user if possible
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}",
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
