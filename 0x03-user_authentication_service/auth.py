65d1bd051f49a04a4693826d09c5d7239f49a00e
William Inyam <sw.inyam@outlook.com>
2024-11-22 01:24:54 +0000

    fix(0x03:auth): fix signature of get_user_from_session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,7 +39,7 @@ class Auth:
             return None
 
     def get_user_from_session_id(
-            self, session_id: str | None = None) -> User | None:
+            self, session_id: str) -> User | None:
         """Finds a user by session_id
 
            Params:
